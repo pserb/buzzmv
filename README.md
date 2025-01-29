27526bf3a71b9db4022ceaec87375667bdbd4c96
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2025-01-29 13:32:55 -0500

    ch9: add user reviews functionality

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Automatically check and format your py files:
 - [x] Chapter 6
 - [x] Chapter 7
 - [x] Chapter 8
-- [ ] Chapter 9
+- [x] Chapter 9
 - [ ] Chapter 10
 - [ ] Chapter 11
 - [ ] Chapter 12
--- moviesstore/movies/admin.py
+++ moviesstore/movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review
 
 
 class MovieAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class MovieAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review)
