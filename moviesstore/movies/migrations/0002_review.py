184c3a9e64ce44f43c5e28f3c2ef7c024912c539
WenweiXu30 <wenweixu0@gmail.com>
2025-01-30 15:10:34 -0500

    C9: Shopping cart, adding and removing items, calculating price

--- moviesstore/movies/migrations/0002_review.py
+++ moviesstore/movies/migrations/0002_review.py
@@ -6,21 +6,31 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('movies', '0001_initial'),
+        ("movies", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Review',
+            name="Review",
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False)),
-                ('comment', models.CharField(max_length=255)),
-                ('date', models.DateTimeField(auto_now_add=True)),
-                ('movie', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='movies.movie')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ("id", models.AutoField(primary_key=True, serialize=False)),
+                ("comment", models.CharField(max_length=255)),
+                ("date", models.DateTimeField(auto_now_add=True)),
+                (
+                    "movie",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="movies.movie"
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
