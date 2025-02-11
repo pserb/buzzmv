ea15074654b4cfabe6c7fe6d58dc39e7b4903ccf
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2025-02-11 16:21:55 -0500

    load environment variables from .env for email

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,5 +141,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "buzzmv0@gmail.com"
-EMAIL_HOST_PASSWORD = "YUM"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
